b862b05bb18617edd9a57ace22b59b94fad0b447
azrielfatur <azriel.fatur1@gmail.com>
2021-10-04 14:49:19 +0700

    adding alert notify

--- src/pages/Components/Alert.vue
+++ src/pages/Components/Alert.vue
@@ -6,7 +6,7 @@
         Utilities for setting the maximum width of an element
       </p>
 
-      <div class="tw-space-y-5">
+      <div class="tw-space-y-5 tw-my-5">
         <div class="tw-bg-white tw-rounded">
           <div class="md:tw-p-5 tw-p-4 tw-pb-1">
             <div class="tw-font-medium">Basic</div>
@@ -32,7 +32,7 @@
                 tw-rounded tw-font-light tw-space-x-3 tw-px-6 tw-py-4 tw-my-5
               "
             >
-              <q-icon name="warning" size="18px" />
+              <q-icon name="error" size="18px" />
               This is a red Alert!
             </div>
           </div>
@@ -59,7 +59,7 @@
                 size="8px"
               />
             </div>
-            <div v-show="!visible" class="md:tw-py-5">
+            <div v-show="!visible" class="tw-py-5">
               <q-btn
                 flat
                 color="indigo-8"
@@ -70,6 +70,18 @@
             </div>
           </div>
         </div>
+
+        <div class="tw-bg-white tw-rounded">
+          <div class="md:tw-p-5 tw-p-4 tw-pb-1">
+            <div class="tw-font-medium">Notify Alert</div>
+            <div class="tw-my-5 tw-space-x-2">
+              <q-btn no-caps unelevated color="positive" label="Notify Positive" @click="positive" />
+              <q-btn no-caps unelevated color="negative" label="Notify Negative" @click="negative" />
+              <q-btn no-caps unelevated color="warning" label="Notify Warning" @click="warning" />
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
 
@@ -86,16 +98,45 @@
     </div>
   </div>
 
-  <!-- <router-view /> -->
 </template>
 
 <script>
+import { ref } from 'vue';
 export default {
   setup() {
     return {
-      visible: true,
-    };
+      visible: ref(true)
+    }
   },
+  methods: {
+    positive () {
+      this.$q.notify({
+        color: 'positive',
+        progress: true,
+        textColor: 'white',
+        icon: 'check',
+        message: 'This is positive Notify Alert! '
+      })
+    },
+    negative () {
+      this.$q.notify({
+          color: 'negative',
+          progress: true,
+          textColor: 'white',
+          icon: 'error',
+          message: 'This is negative Notify Alert! '
+      })
+    },
+    warning () {
+      this.$q.notify({
+          color: 'warning',
+          progress: true,
+          textColor: 'white',
+          icon: 'warning',
+          message: 'This is warning Notify Alert! '
+      })
+    }
+  }
 };
 </script>
 

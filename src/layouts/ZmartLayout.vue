c28c048cd37d680a25a858a021774b92f912d218
azrielfatur <azriel.fatur1@gmail.com>
2021-10-04 13:50:44 +0700

    adding dropdown menu and add menu forms

--- src/layouts/ZmartLayout.vue
+++ src/layouts/ZmartLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-layout view="lHh lpR lFf" style="background-color: #f1f1f1">
+  <q-layout view="lHh LpR lFf" style="background-color: #f1f1f1">
     <q-header class="tw-bg-white tw-text-blue-800">
       <q-toolbar class="tw-my-3 tw-px-5 justify-between">
         <div class="tw-flex">
@@ -25,26 +25,66 @@
           </q-input>
         </div>
 
-        <div class="tw-space-x-5 flex">
-          <q-btn dense flat icon="o_notifications" class="tw-bg-white tw-p-2" />
-          <q-btn dense flat class="tw-bg-white tw-py-2 tw-px-3 tw-font-bold"
-            >AF</q-btn
-          >
-          <div class="tw-flex tw-items-center tw-space-x-4 md">
-            <div class="tw-flex tw-flex-col">
-              <span class="tw-font-medium">Azriel Faturrachman</span>
-              <span class="tw-text-xs tw-font-light"
-                >azriel.fatur1@gmail.com</span
-              >
-            </div>
-            <q-icon name="expand_more" />
+        <div class="tw-space-x-2 flex">
+          <q-btn dense flat icon="o_notifications" class="tw-bg-white tw-p-2">
+            <q-menu class="no-shadow" anchor="bottom right" self="top right">
+              <q-list bordered separator style="min-width: 150px">
+                
+                <q-item clickable v-ripple class="tw-py-3">
+                  <q-item-section avatar>
+                    <q-icon color="negative" text-color="white" name="gpp_bad" />
+                  </q-item-section>
+
+                  <q-item-section>
+                    <q-item-label><strong>Your account was Suspended!</strong></q-item-label>
+                    <q-item-label caption>October 2, 2021</q-item-label>
+                  </q-item-section>
+                </q-item>
+
+                <q-item clickable v-ripple class="tw-py-3">
+                  <q-item-section avatar>
+                    <q-icon color="primary" text-color="white" name="folder" />
+                  </q-item-section>
+
+                  <q-item-section>
+                    <q-item-label><strong>File was an uploaded successfully!</strong></q-item-label>
+                    <q-item-label caption>July 27, 2020</q-item-label>
+                  </q-item-section>
+                </q-item>
+
+              </q-list>
+            </q-menu>
+          </q-btn>
+
+          <div class="tw-flex tw-items-center">
+            <q-btn dense flat class="tw-bg-white tw-py-2 tw-px-3 tw-font-bold md:tw-hidden">AF</q-btn>
+            <q-btn flat no-caps class="tw-flex tw-flex-col md">
+              <div class="tw-mr-3 ">Azriel Faturrachman</div>
+              <q-icon name="expand_more" size="18px"/>
+            </q-btn>
+
+            <q-menu anchor="bottom right" self="top right">
+              <q-list flat style="min-width: 150px">
+                <q-item clickable>
+                  <q-item-section>Profile</q-item-section>
+                </q-item>
+                <q-item clickable>
+                  <q-item-section>Settings</q-item-section>
+                </q-item>
+                <q-separator />
+                <q-item clickable>
+                  <q-item-section>Logout</q-item-section>
+                </q-item>
+              </q-list>
+            </q-menu>
           </div>
+          
         </div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" :width="240">
-      <div class="tw-mx-6 tw-font-light">
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" :width="260">
+      <div class="tw-mx-4 tw-font-light">
         <div class="tw-uppercase text-center tw-text-xl tw-my-10">
           <span class="tw-font-bold">Smart</span> Board
         </div>
@@ -76,16 +116,19 @@
             <q-list class="tw-mx-4">
               <ul class="tw-list-none tw-space-y-3 tw-text-xs">
                 <li>
-                  <router-link to="/form/input" class="tw-no-underline">Input</router-link>
+                  <router-link to="/forms/input" class="tw-no-underline">Input</router-link>
                 </li>
                 <li>
-                  <router-link to="/form/select" class="tw-no-underline">Select</router-link>
+                  <router-link to="/forms/select" class="tw-no-underline">Select</router-link>
                 </li>
                 <li>
-                  <router-link to="/form/radio" class="tw-no-underline">Radio</router-link>
+                  <router-link to="/forms/radio" class="tw-no-underline">Radio</router-link>
                 </li>
                 <li>
-                  <router-link to="/form/checkbox" class="tw-no-underline">Checkbox</router-link>
+                  <router-link to="/forms/checkbox" class="tw-no-underline">Checkbox</router-link>
+                </li>
+                <li>
+                  <router-link to="/forms/form" class="tw-no-underline">Form</router-link>
                 </li>
               </ul>
             </q-list>
